ebe372a1c6c8873aa24ba02b23d7ec4b262d5616
Fernando Sanz <fernandosanzeleven@gmail.com>
2018-01-28 23:26:41 +0100

    Add pruebas con pantallas, movimiento, etc.

--- TR_201718/src/mgf/tr/Main.java
+++ TR_201718/src/mgf/tr/Main.java
@@ -5,6 +5,10 @@
  */
 package mgf.tr;
 
+import java.awt.Color;
+import kp.jngg.Display;
+import kp.jngg.DisplayMode;
+
 /**
  *
  * @author ferna
@@ -15,7 +19,18 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        Display win = Display.create("Título", DisplayMode.getDisplayMode(800, 600));
+        System.out.println(DisplayMode.getAllDisplayModes());
+        
+        win.setGameLoop(new Prueba());
+        win.getDebugInfo().setEnabled(true);
+        win.getDebugInfo().setColor(Color.white);
+        win.getDebugInfo().setExactFps(true);
+        win.getDebugInfo().setShowMemory(true);
+        win.start();
+        
     }
     
 }
